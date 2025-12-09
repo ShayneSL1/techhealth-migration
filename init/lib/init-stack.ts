384783838510c74b72e0905cb39558f6f75113b8
ShayneSL1 <ssluangvisa@gmail.com>
2025-12-09 01:43:45 -0500

    Set up AZs, and subnet configurations for the vpc within lib/init-stack.ts

--- init/lib/init-stack.ts
+++ init/lib/init-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib/core';
+import * as ec2 from '@aws-cdk/aws-ec2'
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -7,7 +8,20 @@ export class InitStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'healthTechVpc', {
+      maxAzs: 2,
+      subnetConfiguration: [{
+        name: 'Public',
+        subnetType: ec2.SubnetType.PUBLIC,
+        cidrMask: 24
+      },
+      {
+        name: 'Private',
+        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        cidrMask: 24
+      }],
 
+    })
     // example resource
     // const queue = new sqs.Queue(this, 'InitQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
