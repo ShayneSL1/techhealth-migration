ae3357941d2a3cfa8b82e6a44407c58f3298fab1
ShayneSL1 <ssluangvisa@gmail.com>
2025-12-09 13:20:08 -0500

    Feature: Added cidr range for the vpc, a security group to the public subnets to allow SSH as shown by const securityGroup to allow outbound traffic, and specific ingress rule to allow only myself to SSH into the EC2 instances. Finally added new EC2 instances of T2 Micro in each public subnet by gathering all publicSubnets ID's then specifying those IDs in the ec2 properties.

--- init/lib/init-stack.ts
+++ init/lib/init-stack.ts
@@ -9,6 +9,7 @@ export class InitStack extends cdk.Stack {
 
     // The code that defines your stack goes here
     const vpc = new ec2.Vpc(this, 'healthTechVpc', {
+      cidr: '10.0.0.0/16',
       maxAzs: 2,
       subnetConfiguration: [{
         name: 'Public',
@@ -21,10 +22,26 @@ export class InitStack extends cdk.Stack {
         cidrMask: 24
       }],
 
-    })
-    // example resource
-    // const queue = new sqs.Queue(this, 'InitQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    });
+
+    const securityGroup = new ec2.SecurityGroup(this, 'InstanceSG', {
+      vpc,
+      description: 'Allow SSH access',
+      allowAllOutbound: true,
+    });
+
+    securityGroup.addIngressRule(ec2.Peer.ipv4(''), ec2.Port.tcp(22), 'Allow SSH'); //Ingress rule only for specific IP address
+
+    const publicSubnets = vpc.selectSubnets({subnetType: ec2.SubnetType.PUBLIC});
+
+    const instance = new ec2.Instance(this, 'MyInstance', {
+      vpc: vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      machineImage: ec2.MachineImage.latestAmazonLinux2023(), // Latest Amazon Linux 2023
+      securityGroup: securityGroup,
+       vpcSubnets: {
+        subnets: publicSubnets.subnets,
+      },
+    });
   }
 }
